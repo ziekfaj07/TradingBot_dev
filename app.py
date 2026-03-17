41d48eaaaee90956afd3fb94a68f9d484416af4c
ziekfaj07 <exe.fajardo@gmail.com>
2026-03-17 15:17:58 +0800

    trial commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from routers.backtest_router import router as backtest_router
 from routers.export_router import router as export_router
 from routers.run_router import router as run_router
 from routers.ws_router import router as ws_router
+from services.exchange_service import connect_exchange
 
 app = FastAPI()
 
