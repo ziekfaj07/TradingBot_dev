e5eea838a2ad6cd6d87e3b882898778e0bf4e235
ziekfaj07 <exe.fajardo@gmail.com>
2026-03-27 10:20:03 +0800

    more v0.4.5 fix attempt

--- app.py
+++ app.py
@@ -5,11 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 
 from routers.dashboard_router import router as dashboard_router
-from routers.backtest_router import router as backtest_router
 from routers.export_router import router as export_router
 from routers.run_router import router as run_router
 from routers.ws_router import router as ws_router
-from routers.paper_router import router as paper_router
 
 from services.exchange_service import connect_exchange
 
